169fb549d8044553cca6c00f93e69f69abedd9e4
ruwaidah <Ruwidah.riyadh@gmail.com>
2019-12-21 00:09:41 -0600

    updating

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const server = require("./api/server");
 
 const port = process.env.PORT || 5000;
 
+server.get("/", (req, res) => {
+  res.send("<h1>Welcome</h1>");
+});
+
 server.listen(port, () => console.log(`server is running on port ${port}`));
