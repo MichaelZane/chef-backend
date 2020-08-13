0cb4a464236a94107ca52dd462d637e53f6166c1
Michael Stevens <stevensmike36@yahoo.com>
2020-08-12 23:17:39 -0500

    .

--- endpoints/login/login-model.js
+++ endpoints/login/login-model.js
@@ -1,52 +1,52 @@
 const db = require("../../database/db-config.js");
 
-// module.exports = {
-//   findUser,
-//   findById
-// };
-
-// function findUser(username) {
-//   return db("users")
-//     .where({ username })
-//     .first();
-// }
-
-// function findById(id) {
-//   console.log(id);
-//   return db("users")
-//     .where({ id: id })
-//     .first();
-// }
 module.exports = {
-  add,
-  find,
-  findBy,
-  findById,
-  remove
+  findUser,
+  findById
 };
 
-function add(user) {
+function findUser(username) {
   return db("users")
-  .returning(['id', 'username', 'email' ])
-  .insert(user);
-}
-
-function find() {
-  return db("users").select("id", "username", "email");
-}
-
-function findBy(filter) {
-  return db("users").where(filter);
-}
-
-function findById(id) {
-  return db("users")
-    .where({ id })
+    .where({ username })
     .first();
 }
 
-function remove(id) {
+function findById(id) {
+  console.log(id);
   return db("users")
-    .where({ id })
-    .del();
+    .where({ id: id })
+    .first();
 }
+// module.exports = {
+//   add,
+//   find,
+//   findBy,
+//   findById,
+//   remove
+// };
+
+// function add(user) {
+//   return db("users")
+//   .returning(['id', 'username', 'email' ])
+//   .insert(user);
+// }
+
+// function find() {
+//   return db("users").select("id", "username", "email");
+// }
+
+// function findBy(filter) {
+//   return db("users").where(filter);
+// }
+
+// function findById(id) {
+//   return db("users")
+//     .where({ id })
+//     .first();
+// }
+
+// function remove(id) {
+//   return db("users")
+//     .where({ id })
+//     .del();
+// }
