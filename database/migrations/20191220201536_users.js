203f270e4d1e05e12f3b8461c20e67cad03b19fa
Michael Stevens <stevensmike36@yahoo.com>
2020-08-12 23:11:35 -0500

    server crashed db reset, fixed login

--- database/migrations/20191220201536_users.js
+++ database/migrations/20191220201536_users.js
@@ -1,20 +1,24 @@
 exports.up = function(knex) {
   return knex.schema.createTable("users", tbl => {
     tbl.increments();
-    tbl.string("firstname", 100).notNullable();
-    tbl.string("lastname", 100).notNullable();
+    tbl.string("firstname", 100)
+    .notNullable();
+    tbl.string("lastname", 100)
+    .notNullable();
     tbl
       .string("username", 100)
       .notNullable()
       .unique();
-    tbl.string("password", 100).notNullable();
+    tbl.string("password", 100)
+    .notNullable();
     tbl
       .string("email", 100)
       .notNullable()
       .unique();
     tbl.string("city", 100);
     tbl.string("state", 100);
-    tbl.integer("phone", 100).unique();
+    tbl.integer("phone", 100)
+    .unique();
     tbl.string("address", 100);
     tbl.integer("zipcode", 100);
   });
--- endpoints/login/loginRouter.js
+++ endpoints/login/loginRouter.js
@@ -6,137 +6,140 @@ const jwt = require("jsonwebtoken");
 const Users = require("./login-model.js");
 
 // LogIn EndPoint
-// router.post("/", (req, res) => {
-//   const { username, password } = req.body;
-//   if (username && password) {
-//     Users.findUser(username)
-//       .then(user => {
-//         if (user) {
-//           if (bcrypt.compareSync(req.body.password, user.password)) {
-//             const token = generateToken(user);
-//             res.status(200).json({
-//               token,
-//               user: {
-//                 id: user.id,
-//                 firstname: user.firstname,
-//                 lastname: user.lastname,
-//                 username: user.username,
-//                 city: user.city,
-//                 state: user.state,
-//                 phone: user.phone,
-//                 email: user.email,
-//                 address: user.address,
-//                 zipcode: user.zipcode
-//               }
-//             });
-//           } else {
-//             res.status(400).json({
-//               message: "username or password is incorrect!"
-//             });
-//           }
-//         } else {
-//           res.status(400).json({
-//             message: "username or password is incorrect!"
-//           });
-//         }
-//       })
-//       .catch(error => {
-//         res.status(500).json({
-//           message: "error getting the user"
-//         });
-//       });
-//   } else {
-//     res.status(404).json({
-//       message: "Please Provide username and password !"
-//     });
-//   }
-// });
-
-// function generateToken(user) {
-//   const payload = {
-//     username: user.username
-//   };
-
-//   const secret = process.env.JWT_SECRET || "is it secret, is it safe?";
-
-//   const options = {
-//     expiresIn: "1d"
-//   };
-//   return jwt.sign(payload, secret, options);
-// }
-// logs in a user
 router.post("/", (req, res) => {
-  let { username, password } = req.body;
-
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user && bc.compareSync(password, user.password)) {
-        const token = generateToken(user);
-        const user_id = user.id;
-        const email = user.email;
-        res
-          .status(200)
-          .json({ message: `Welcome ${user.username}!`, token, user_id, email });
-      } else {
-        res.status(401).json({ message: "Invalid Credentials" });
-      }
-    })
-    .catch(error => {
-      console.log(error);
-      res.status(500).json(error);
+  const { username, password } = req.body;
+  if (username && password) {
+    Users.findUser(username)
+      .then(user => {
+        if (user) {
+          if (bcrypt.compareSync(req.body.password, user.password)) {
+            const token = generateToken(user);
+            res.status(200).json({
+              token,
+              user: {
+                id: user.id,
+                firstname: user.firstname,
+                lastname: user.lastname,
+                username: user.username,
+                city: user.city,
+                state: user.state,
+                phone: user.phone,
+                email: user.email,
+                address: user.address,
+                zipcode: user.zipcode
+              }
+            });
+          } else {
+            res.status(400).json({
+              message: "username or password is incorrect!"
+            });
+          }
+        } else {
+          res.status(400).json({
+            message: "username or password is incorrect!"
+          });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({
+          message: "error getting the user"
+        });
+      });
+  } else {
+    res.status(404).json({
+      message: "Please Provide username and password !"
     });
+  }
 });
 
-// Gets all users
-router.get("/", (req, res) => {
-  Users.find()
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => {
-      res.status(500).json({ message: "Failed to get all users" });
-    });
-});
-
-// Gets user by id
-router.get("/:id", (req, res) => {
-  Users.findById(req.params.id)
-    .then(user => {
-      res.status(200).json(user);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({ message: "failed to get user" });
-    });
-});
-
-// Removes a user by id
-router.delete("/:id", (req, res) => {
-  const { id } = req.params;
-
-  Users.remove(id)
-    .then(deleted => {
-      if (deleted) {
-        res.json({ removed: deleted });
-      } else {
-        res.status(404).json({ message: "Could not find user with given id" });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ message: "Failed to delete the user" });
-    });
-});
-
-// Generates JWT
 function generateToken(user) {
   const payload = {
-    username: user.username,
-    id: user.id
+    username: user.username
   };
+
+  const secret = process.env.JWT_SECRET || "is it secret, is it safe?";
+
   const options = {
     expiresIn: "1d"
   };
-  return jwt.sign(payload, process.env.JWT_SECRET || "letsQuest", options);
+  return jwt.sign(payload, secret, options);
 }
+
 module.exports = router;
+
+// logs in a user
+// router.post("/", (req, res) => {
+//   let { username, password } = req.body;
+
+//   Users.findBy({ username })
+//     .first()
+//     .then(user => {
+//       if (user && bc.compareSync(password, user.password)) {
+//         const token = generateToken(user);
+//         const user_id = user.id;
+//         const email = user.email;
+//         res
+//           .status(200)
+//           .json({ message: `Welcome ${user.username}!`, token, user_id, email });
+//       } else {
+//         res.status(401).json({ message: "Invalid Credentials" });
+//       }
+//     })
+//     .catch(error => {
+//       console.log(error);
+//       res.status(500).json(error);
+//     });
+// });
+
+// // Gets all users
+// router.get("/", (req, res) => {
+//   Users.find()
+//     .then(users => {
+//       res.json(users);
+//     })
+//     .catch(err => {
+//       res.status(500).json({ message: "Failed to get all users" });
+//     });
+// });
+
+// // Gets user by id
+// router.get("/:id", (req, res) => {
+//   Users.findById(req.params.id)
+//     .then(user => {
+//       res.status(200).json(user);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json({ message: "failed to get user" });
+//     });
+// });
+
+// // Removes a user by id
+// router.delete("/:id", (req, res) => {
+//   const { id } = req.params;
+
+//   Users.remove(id)
+//     .then(deleted => {
+//       if (deleted) {
+//         res.json({ removed: deleted });
+//       } else {
+//         res.status(404).json({ message: "Could not find user with given id" });
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).json({ message: "Failed to delete the user" });
+//     });
+// });
+
+// // Generates JWT
+// function generateToken(user) {
+//   const payload = {
+//     username: user.username,
+//     id: user.id
+//   };
+//   const options = {
+//     expiresIn: "1d"
+//   };
+//   return jwt.sign(payload, process.env.JWT_SECRET || "letsQuest", options);
+// }
+// module.exports = router;
